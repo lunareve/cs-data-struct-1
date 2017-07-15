5e9341880067aee7d1c01d53ae3378d9e7c0b6d4
lunareve <clionesse@yahoo.com>
2017-07-15 23:49:13 +0000

    initial commit

---
+++ discussion.py
@@ -0,0 +1,28 @@
+"""
+Runtime
+1. The workload is the part of your algorithm that grows at the fastest rate as the workload approaches infinity. Constants and factors are not included in this calculation as they don't change.
+2.
+O(1)
+O(log n)
+O(n)
+O(n log n)
+O(n^2)
+O(2^n)
+
+Stacks and Queues
+1.1. stack
+1.2. queue
+1.3. stack
+2. an egg skelter, drip coffee
+3. putting out a forest fire, planting a tree
+
+Linked Lists
+1. The whole of the box encompassing fruit and next is a node. The data is the text apple, berry or cherry. The head is the box, text, and arrow labeled head.
+2. A double linked list has arrows pointing to the previous item and next items and has a bit more overhead. A single linked list only points in one direction.
+3. Without a tail, we would need to traverse the entire list. With the tail, we can get to the end directly and modify the list from there.
+
+Trees
+1. food, Italian, Indian, Mexican, lasagna, pizza, tikka masala, saag, burritos
+2. food, Mexican, enchiladas, tacos, burritos, Indian, saag, tikka masala, Italian, pizza, Sicilian, New York-style, Chicago-style
+3. A binary search tree needs to be balanced and ordered. Its root should be the middle of the nodes that make up the tree.
+"""
